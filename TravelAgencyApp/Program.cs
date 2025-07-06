fa0c6f9e4114e4ee31c648c14334c0bb9ddc215d
FilipZapletal <f.zapletal0@seznam.cz>
2025-07-06 23:25:59 +0200

    changed connection to online database

--- TravelAgencyApp/Program.cs
+++ TravelAgencyApp/Program.cs
@@ -7,8 +7,10 @@ using TravelAgencyApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MonsterDbConnection")));
 builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false;
